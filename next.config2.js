421e93ac36f73f84aa37c08b1717b1a97d11d6ba
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 17:37:54 -0300

    done

---
+++ next.config2.js
@@ -0,0 +1,14 @@
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    APP_NAME: "SEOBLOG",
+    API_DEVELOPMENT: "https://blogusa.herokuapp.com/api",
+    API_PRODUCTION: "https://blogusa.herokuapp.com/api",
+    PRODUCTION: "true",
+    DOMAIN_DEVELOPMENT:'https://blogusa.herokuapp.com/api',
+    DOMAIN_PRODUCTION:'https://blogusa.herokuapp.com/api',
+    FB_APP_ID:'703266223770406',
+    DISQUS_SHORTNAME:'plantation',
+  },
+});
