6ac269d2453172e6119a6257b777016a6977fcba
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 16:33:18 -0300

    done

--- config.js
+++ config.js
@@ -11,5 +11,5 @@ export const DOMAIN = 'https://blogusa.herokuapp.com/api'
   // ? publicRuntimeConfig.DOMAIN_PRODUCTION
   // : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
-export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME
+export const FB_APP_ID = '703266223770406';
+export const DISQUS_SHORTNAME ='plantation'
