b5a976848555e21f5652b46841e30bd1364a0bd5
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 15:38:55 -0300

    foi

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import "../node_modules/react-quill/dist/quill.snow.css";
 import "../static/css/styles.css";
 import "../node_modules/nprogress/nprogress.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
