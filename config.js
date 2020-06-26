1567ca696ecf5a63b9b95eb7c1ff9272b528806d
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 16:28:11 -0300

    done

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ export const API = "https://blogusa.herokuapp.com/api"
   // : publicRuntimeConfig.API_DEVELOPMENT;
 export const APP_NAME = 'https://blogusa.herokuapp.com/api'
 
-export const DOMAIN = publicRuntimeConfig.PRODUCTION
+export const DOMAIN = 'https://blogusa.herokuapp.com/api'
   // ? publicRuntimeConfig.DOMAIN_PRODUCTION
   // : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
