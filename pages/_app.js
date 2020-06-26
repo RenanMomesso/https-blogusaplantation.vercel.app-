ef35d54a1cded1e8ab9f213f2298928d97a34496
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 15:29:40 -0300

    foi

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import "../../node_modules/react-quill/dist/quill.snow.css";
+import "../node_modules/react-quill/dist/quill.snow.css";
 import "../static/css/styles.css";
-import ".././node_modules/nprogress/nprogress.css";
+import "../node_modules/nprogress/nprogress.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'any-other-css-you-want.css';
 
