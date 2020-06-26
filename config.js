af8b6b6b834f2533bf0718e09cfcae16cf32b584
RenanMomesso <renan_momesso@hotmail.com>
2020-06-26 17:33:20 -0300

    done

--- config.js
+++ config.js
@@ -1,32 +1,15 @@
-import getConfig from "next/config";
+import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-const withCSS = require("@zeit/next-css");
+export const API = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.API_PRODUCTION
+    : publicRuntimeConfig.API_DEVELOPMENT;
 
-module.exports = withCSS({
-  publicRuntimeConfig: {
-    APP_NAME: "SEOBLOG",
-    API_DEVELOPMENT: "https://blogusa.herokuapp.com/api",
-    API_PRODUCTION: "https://blogusa.herokuapp.com/api",
-    PRODUCTION: "true",
-    DOMAIN_DEVELOPMENT:'https://blogusa.herokuapp.com/api',
-    DOMAIN_PRODUCTION:'https://blogusa.herokuapp.com/api',
-    FB_APP_ID:'703266223770406',
-    DISQUS_SHORTNAME:'plantation',
-  },
-});
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+    ? publicRuntimeConfig.DOMAIN_PRODUCTION
+    : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
 
-
-
-export const API = publicRuntimeConfig.API_PRODUCTION;
-  // ? publicRuntimeConfig.API_PRODUCTION
-  // : publicRuntimeConfig.API_DEVELOPMENT;
-export const APP_NAME = publicRuntimeConfig.APP_NAME
-
-export const DOMAIN = publicRuntimeConfig.DOMAIN
-  // ? publicRuntimeConfig.DOMAIN_PRODUCTION
-  // : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
-
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID
-export const DISQUS_SHORTNAME =publicRuntimeConfig.DISQUS_SHORTNAME
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
